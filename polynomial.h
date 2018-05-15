8477ab2936f328e6c3d23a5b0e512758b09f61f9
hiiragi4000 <b02401117@ntu.edu.tw>
2018-05-16 04:42:27 +0800

    allow the access to f[i] for i>=f.size()

--- polynomial.h
+++ polynomial.h
@@ -102,9 +102,9 @@ private:
         assert(m == b.m);
         int d1=deg(), d2=b.deg();
         if(d1<0 || d2<0){
-            return polynomial<Z>(0, m);
+            return polynomial<Z>(m);
         }
-        polynomial<Z> result(d1+d2+1, m);
+        polynomial<Z> result(m); result.resize(d1+d2+1);
         for(int i=0; i<=d1; i++) for(int j=0; j<=d2; j++){
             if(m){
                 result[i+j] = (result[i+j]+a[i]*b[j]%m)%m;
@@ -122,9 +122,9 @@ private:
         int d1=r.deg(), d2=g.deg();
         assert(d2>=0 && abs(g[d2])==1);
         if(d1 < d2){
-            return make_pair(polynomial<Z>(0, m), r);
+            return make_pair(polynomial<Z>(m), r);
         }
-        polynomial<Z> q(d1-d2+1, m);
+        polynomial<Z> q(m); q.resize(d1-d2+1);
         for(int i=d1-d2; i>=0; i--){
             q[i] = g[d2] * r[d2+i];
             for(int j=0; j<=d2; j++){
@@ -139,9 +139,8 @@ private:
         return make_pair(q, r);
     }
 public:
-    polynomial(const size_t &n=0, const Z &m=0): m(m){
+    polynomial(const Z &m=0): m(m){
         assert(m>=0 && m!=1);
-        a = vector<Z>(n);
     }
     polynomial(const vector<Z> &coef, const Z &m=0): m(m){
         assert(m>=0 && m!=1);
@@ -153,13 +152,14 @@ public:
     size_t size() const{
         return a.size();
     }
-    Z& operator [](const int &i){
-        assert(0<=i && i<(int)a.size());
+    Z &operator [](const int &i){
+        assert(i >= 0);
+        if(i >= (int)a.size()) a.resize(i+1);
         return a[i];
     }
-    const Z& operator [](const int &i) const{
-        assert(0<=i && i<(int)a.size());
-        return a[i];
+    const Z operator [](const int &i) const{
+        assert(i >= 0);
+        return (i<(int)a.size())? a[i]: 0;
     }
     Z mod() const{
         return m;
@@ -201,14 +201,17 @@ public:
     polynomial<Z> operator -() const{
         polynomial<Z> result = *this;
         result.trunc();
-        for(int i=0; i<(int)result.size(); i++){
-            if(m){
-                result[i] = m-result[i];
-            }else{
-                result[i] = -result[i];
+        if(m){
+            for(int i=0; i<(int)result.size(); i++){
+                if(result[i]){
+                    result[i] = m-result[i];
+                }
+            }
+        }else{
+            for(int i=0; i<(int)result.size(); i++){
+                result[i] *= (-1);
             }
         }
-        result.trunc();
         return result;
     }
     polynomial<Z> operator -(const polynomial<Z> &b) const{
@@ -238,14 +241,15 @@ public:
             return osoi_kakezan(b);
         }
         int n = max(d1, d2)/2 + 1;
-        polynomial<Z> A(n, m), B(n, m), C(n, m), D(n, m);
+        polynomial<Z> A(m), B(m), C(m), D(m);
+        A.resize(n), B.resize(n), C.resize(n), D.resize(n);
         for(int i=0; i<=n-1&&i<(int)size(); i++) B[i] = a[i];
         for(int i=0; i<=n-1&&n+i<(int)size(); i++) A[i] = a[n+i];
         for(int i=0; i<=n-1&&i<(int)b.size(); i++) D[i] = b[i];
         for(int i=0; i<=n-1&&n+i<(int)b.size(); i++) C[i] = b[n+i];
         A.trunc(); B.trunc(); C.trunc(); D.trunc();
         polynomial<Z> E=A.karatsuba(C), F=B.karatsuba(D), G=(A+B).karatsuba(C+D)-E-F;
-        polynomial<Z> result(d1+d2+1, m);
+        polynomial<Z> result(m); result.resize(d1+d2+1);
         for(int i=0; i<(int)E.size(); i++) result[2*n+i] += E[i];
         for(int i=0; i<(int)G.size(); i++) result[n+i] += G[i];
         for(int i=0; i<(int)F.size(); i++) result[i] += F[i];
@@ -277,7 +281,7 @@ public:
         xi = ntt(xi, 1); eta = ntt(eta, 1);
         for(int i=0; i<=n-1; i++) xi[i] = xi[i]*eta[i]%nttp;
         xi = ntt(xi, -1);
-        polynomial<Z> result(n, m);
+        polynomial<Z> result(m); result.resize(n);
         for(int i=0; i<=n-1; i++){
             long long q = floor((x[i].real()-xi[i])/nttp+.5);
             if(!m){
@@ -328,12 +332,12 @@ public:
     polynomial<Z> invmodxn(const int &n) const{
         assert(n>0 && deg()>=0 && abs(a[0])==1);
         if(n == 1){
-            polynomial<Z> b(1, m);
+            polynomial<Z> b(m);
             b[0]=a[0]; return b;
         }
         int l = (n+1)/2;
         polynomial<Z> p = invmodxn(l);
-        polynomial<Z> a0(l, m), a1(l, m);
+        polynomial<Z> a0(m), a1(m); a0.resize(l), a1.resize(l);
         for(int i=0; i<=l-1&&i<(int)size(); i++){
             a0[i] = a[i];
         }
@@ -343,15 +347,14 @@ public:
         }
         a1.trunc();
         polynomial<Z> a0p = a0*p;
-        polynomial<Z> b(l, m);
+        polynomial<Z> b(m); b.resize(l);
         for(int i=0; i<=l-1&&l+i<(int)a0p.size(); i++){
             b[i] = a0p[l+i];
         }
         b.trunc();
-        polynomial<Z> q = a1*p+b;
-        q.resize(l);
+        polynomial<Z> q = a1*p+b; q.resize(l);
         q *= -p;
-        polynomial<Z> result(n, m);
+        polynomial<Z> result(m); result.resize(n);
         for(int i=0; i<=l-1&&i<(int)p.size(); i++){
             result[i] = p[i];
         }
@@ -366,13 +369,13 @@ public:
         int d1 = deg(), d2 = b.deg();
         assert(d2 >= 0);
         if(d1 < d2){
-            return polynomial<Z>(0, m);
+            return polynomial<Z>(m);
         }
         if(d2 <= 32){
             return osoi_warizan(b).first;
         }
         polynomial<Z> rf=rev(), rg=b.rev(), rq=rf*rg.invmodxn(d1-d2+1);
-        polynomial<Z> q(d1-d2+1, m);
+        polynomial<Z> q(m); q.resize(d1-d2+1);
         for(int i=d1-d2; i>=0&&d1-d2-i<(int)rq.size(); i--){
             q[i] = rq[d1-d2-i];
         }
@@ -403,7 +406,7 @@ public:
         assert(d2 >= 0);
         if(d1 < d2){
             polynomial<Z> r=*this; r.trunc();
-            return make_pair(polynomial<Z>(0, m), r);
+            return make_pair(polynomial<Z>(m), r);
         }
         if(d2 <= 32){
             return osoi_warizan(b);
